b34d783aa7177c30e23025dea7147d4409cbeb46
Jimmy Chang <jimmythechang@gmail.com>
2013-06-24 00:56:32 -0700

    Styling tweaks.

--- application/views/boardgame.php
+++ application/views/boardgame.php
@@ -5,22 +5,22 @@
             <div class="span4">
                 <img src="/assets/img/<?php echo $boardgame->filename; ?>"/>
             </div>
-            <div class="span8">
+            <div id="contentContainer" class="span7">
                 <div id="gameNameContainer" class="editable">
                     <h1 class="text"><?php echo $boardgame->name; ?></h1>
-                    <input type="button" value="Edit" class="editButton btn btn-primary hidden"/>
+                    <input type="button" value="Edit" class="editButton btn hidden"/>
                     <div class="editSection hidden">
                         <input type="text" class="editText"/>
-                        <input type="button" value="Save" class="saveButton btn btn-primary"/>
+                        <input type="button" value="Save" class="saveButton btn"/>
                         <input type="hidden" value="name" class="field"/>
                     </div>
                 </div>
                 <div id="gameDescriptionContainer" class="editable">
                     <p class="text"><?php echo $boardgame->description; ?></p>
-                    <input type="button" value="Edit" class="editButton btn btn-primary hidden"/>
+                    <input type="button" value="Edit" class="editButton btn hidden"/>
                     <div class="editSection hidden">
                         <textarea class="editText" cols="10" rows="5"></textarea>
-                        <input type="button" value="Save" class="saveButton btn btn-primary"/>
+                        <input type="button" value="Save" class="saveButton btn"/>
                         <input type="hidden" value="description" class="field"/>
                     </div>
                 </div>
