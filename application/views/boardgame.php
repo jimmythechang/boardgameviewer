ebee949627ee1952e4fd75930fb4458cd16e0e97
Jimmy Chang <jimmythechang@gmail.com>
2013-06-23 22:01:10 -0700

    Refactored javascript for hiding/showing editable sections.

--- application/views/boardgame.php
+++ application/views/boardgame.php
@@ -1,17 +1,25 @@
 <?php echo View::factory('header'); ?>
-    <div class="container">
+    <div id="boardgameView" class="container">
         <div class="row">
             <div class="span4">
                 <img src="/assets/img/<?php echo $boardgame->filename; ?>"/>
             </div>
             <div class="span8">
-                <div id="gameNameContainer">
-                    <h1><?php echo $boardgame->name; ?></h1>
-                    <input type="button" value="Edit" class="editButton btn btn-primary"/>
+                <div id="gameNameContainer" class="editable">
+                    <h1 class="text"><?php echo $boardgame->name; ?></h1>
+                    <input type="button" value="Edit" class="editButton btn btn-primary hidden"/>
+                    <div class="editSection hidden">
+                        <input type="text" class="editText"/>
+                        <input type="button" value="Save" class="saveButton btn btn-primary"/>
+                    </div>
                 </div>
-                <div id="gameDescriptionContainer">
-                    <p><?php echo $boardgame->description; ?></p>
-                    <input type="button" value="Edit" class="editButton btn btn-primary"/>
+                <div id="gameDescriptionContainer" class="editable">
+                    <p class="text"><?php echo $boardgame->description; ?></p>
+                    <input type="button" value="Edit" class="editButton btn btn-primary hidden"/>
+                    <div class="editSection hidden">
+                        <textarea class="editText" cols="10" rows="5"></textarea>
+                        <input type="button" value="Save" class="saveButton btn btn-primary"/>
+                    </div>
                 </div>
             </div>
         </div>
--- assets/js/boardgame.js
+++ assets/js/boardgame.js
@@ -1,29 +1,55 @@
 $(document).ready(function() {
     bindEditHovers();
+    bindEditClicks();
 });
 
+// Displays an Edit button when an editable div is hovered over.
+
 function bindEditHovers() {
-    // Show the Edit button for the game title if the user hovers
-    // over the div.
-    $('#gameNameContainer').hover(
+    $('.editable').hover(
         function()
         {
-            $('#gameNameContainer .editButton').fadeIn('fast');
+            if ($(this).hasClass('editing')) {
+                return;
+            }
+            
+            $('.editButton', this).fadeIn('fast');
+            $('.editButton', this).removeClass('hidden');
         },
         function()
         {
-            $('#gameNameContainer .editButton').stop().fadeOut('fast');
+            $('.editButton', this).stop().fadeOut('fast');
+            $('.editButton', this).addClass('hidden');
         }
     );
+}
 
-    $('#gameDescriptionContainer').hover(
+function bindEditClicks() {
+    $('.editButton').click(
         function()
         {
-            $('#gameDescriptionContainer .editButton').fadeIn('fast');
-        },
+            var editable = $(this).parent();
+            var text = $('.text', editable).text();
+
+            // Indicate that we're currently editing the div,
+            // so that the Edit button isn't displayed
+            // again on hover.
+            $(editable).addClass('editing');
+
+            $(this).hide().addClass('hidden');
+            $('.text', editable).hide();
+
+            $('.editSection', editable).removeClass('hidden');
+            $('.editText', editable).val(text);
+        }
+    );
+}
+
+function bindSaveClicks() {
+    $('.saveButton').click(
         function()
         {
-            $('#gameDescriptionContainer .editButton').stop().fadeOut('fast');
+
         }
     );
 }
